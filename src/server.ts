41d0965bc7aae0554f04e46fab5316fc112c4b4a
iamrosada <luisrosada2018@gmail.com>
2023-04-27 23:48:22 +0300

    feat: server for pedidos API

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import "./infra/providers/kafka/consumers";
+
+const PORT = process.env.PORT ?? 3002;
+
+const app = express();
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server Pedido is running on PORT ${PORT}`));
