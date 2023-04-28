6550036f31854055bf1720963e22f000281e3d0d
iamrosada <luisrosada2018@gmail.com>
2023-04-28 15:15:10 +0300

    feat: created server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import "./infra/providers/kafka/consumers";
+import { router } from "./infra/routes";
 
 const PORT = process.env.PORT ?? 3002;
 
 const app = express();
 app.use(express.json());
+
+app.use(router);
 
 app.listen(PORT, () => console.log(`Server Pedido is running on PORT ${PORT}`));
